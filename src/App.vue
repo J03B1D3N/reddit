9f32bbf8611866f0b822fbb77a90670cefc7d933
Justas Lapiene <justas63@gmail.com>
2023-06-18 15:37:05 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import "./assets/main.css"
+import Header from "./components/header.vue"
+</script>
+
+<template>
+
+
+<Header></Header>
+
+
+</template>
+
+
---
+++ src/components/header.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <div class="header w-full">
+        lmao
+    </div>
+</template>

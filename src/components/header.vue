3225797efbbbb494ed53b055ad96682167f4a58a
Justas Lapiene <justas63@gmail.com>
2023-06-19 13:36:38 +0200

    added google auth

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,16 +1,81 @@
 <script setup lang="ts">
 import logo from "../assets/Logo.webp"
 import { ref } from "vue";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "firebase/auth";
+import {provider, auth} from "../firebase"
+
 
 const Dropdown = ref(false)
 function handleDropdown() {
     Dropdown.value = !Dropdown.value
 }
+
+const signedIn = ref(false)
+
+function setSignedIn(arg:boolean) {
+  signedIn.value = arg
+}
+
 function close() {
     setTimeout(() => {
         Dropdown.value = false;
-    }, 50);
+    }, 200);
 }
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    console.log("user is signed in")
+    const uid = user.uid;
+    setSignedIn(true)
+    // ...
+  } else {
+    setSignedIn(false)
+
+    // User is signed out
+    console.log('user is signed out')
+    // ...
+  }
+});
+
+
+function handleSignIn() {
+
+  signInWithPopup(auth, provider)
+
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+}
+
+function handleSignOut() {
+  setTimeout(() => {
+    signOut(auth).then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+});
+    }, 200);
+    
+}
+
+
 
 </script>
 
@@ -25,10 +90,10 @@ function close() {
 
         <div class="wrapper flex justify-around gap-3">
             <button class="bg-white hover:bg-slate-300 font-bold py-2 px-4 rounded">Get App</button>
-            <button class="bg-orange-600 hover:bg-orange-900 text-white font-bold py-2 px-4 rounded">Log In</button>
+            <button v-if="!signedIn" @click="handleSignIn" class="bg-orange-600 hover:bg-orange-900 text-white font-bold py-2 px-4 rounded">Log In</button>
             <div class="relative inline-block text-left">
   <div>
-    <button v-bind:class="[Dropdown ? 'bg-orange-900' : '']" @click="handleDropdown" @blur="close" type="button" class="inline-flex w-full justify-center items-center gap-x-1.5 bg-orange-600 hover:bg-orange-900 text-white font-bold py-2 px-4 rounded" id="menu-button" aria-expanded="true" aria-haspopup="true">
+    <button v-if="signedIn" v-bind:class="[Dropdown ? 'bg-orange-900' : '']" @click="handleDropdown" @blur="close" type="button" class="inline-flex w-full justify-center items-center gap-x-1.5 bg-orange-600 hover:bg-orange-900 text-white font-bold py-2 px-4 rounded" id="menu-button" aria-expanded="true" aria-haspopup="true">
       User
       <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
         <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
@@ -38,10 +103,9 @@ function close() {
   
   <div v-if="Dropdown" class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
     <div class="py-1" role="none">
-      <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-      <a href="#" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a>
+      <a href="#" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900" role="menuitem" tabindex="-1" id="menu-item-0">Account Settings</a>
       <a href="#" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900" role="menuitem" tabindex="-1" id="menu-item-1">Support</a>
-      <a href="#" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900" role="menuitem" tabindex="-1" id="menu-item-2">License</a>
+      <button @click="handleSignOut" href="#" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900 text-left" role="menuitem" tabindex="-1" id="menu-item-2">Log Out</button>
       
     </div>
   </div>
---
+++ src/firebase.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getStorage, ref} from "firebase/storage"
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANGZ0SJUBUMGNmC0xcNv_GMHeZQNBIH90",
+  authDomain: "reddot-ce765.firebaseapp.com",
+  projectId: "reddot-ce765",
+  storageBucket: "reddot-ce765.appspot.com",
+  messagingSenderId: "755205730654",
+  appId: "1:755205730654:web:89a69cebdd1010bf251fcf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const storage = getStorage(app);
+
+const strogeRef = ref(storage)
+
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+
+
+  export {auth, provider}
+
